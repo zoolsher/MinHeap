6087fdee4e6e8b9fb4208bfce5672214f0faa2ce
zoolsher <zoolsher@gmail.com>
2016-04-17 00:36:14 +0800

    add remove function

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -80,6 +80,49 @@ vector<V> BinarySearchTree<K,V>::search(K min, K max) {
     return v;
 }
 
+template <class K,class V>
+void BinarySearchTree<K,V>::remove(K key,V value) {
+    this->head = this->remove(key, value,this->head);
+}
+
+template <class K,class V>
+inline Node<K,V>* BinarySearchTree<K,V>::findMin(Node<K,V> *curHead){
+    Node<K,V> *temp = curHead;
+    while(temp->leftChild!=NULL){
+        temp = temp->leftChild;
+    }
+    return temp;
+};
+
+
+template <class K,class V>
+Node<K,V>* BinarySearchTree<K,V>::remove(K key,V value,Node<K,V> *curHead){
+    if(curHead == NULL){
+        return curHead;
+    }
+    if(curHead->key==key){
+        if(curHead->leftChild==NULL && curHead->rightChild==NULL){
+            return NULL;
+        }else{
+            if(curHead->leftChild!=NULL && curHead->rightChild!=NULL){
+                Node<K,V> *min = this->findMin(curHead->rightChild);
+                min->rightChild = this->remove(min->key,min->value,curHead->rightChild);
+                min->leftChild = curHead->leftChild;
+                return min;
+            }else{
+                return curHead->rightChild==NULL?curHead->leftChild:curHead->rightChild;
+            }
+        }
+    }else{
+        if(curHead->key>key){
+            curHead->leftChild = this->remove(key,value,curHead->leftChild);
+        }else{
+            curHead->rightChild = this->remove(key,value,curHead->rightChild);
+        }
+        return curHead;
+    }
+};
+
 
 template <class K,class V>
 void BinarySearchTree<K,V>::search(K min,K max,vector<V> *v,Node<K,V> *curHead) {
--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -19,11 +19,14 @@ public:
     void insert(Node<K,V> *node);
     vector<V> search(K min,K max);
     void print();
+    void remove(K key,V value);
 private:
     int getLength(Node<K,V> *cur);
     void insert(Node<K,V> *node,Node<K,V> *curHead);
     void print(Node<K,V> *cur, vector<Node<K,V>*> *v);
     void search(K min,K max,vector<V> *v,Node<K,V> *curHead);
+    Node<K,V>* remove(K key,V value,Node<K,V> *curHead);
+    inline Node<K,V>* findMin(Node<K,V> *curHead);
 };
 
 
